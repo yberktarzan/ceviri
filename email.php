226dc8b374f06cef116766606b55394823675a4d
yberktarzan <43090597+yberktarzan@users.noreply.github.com>
2021-07-12 06:48:57 +0300

    Upload
    upload language files

---
+++ email.php
@@ -0,0 +1,401 @@
+<?php
+
+return array(
+  'newEvent' =>
+  array(
+    'subject' => 'New Event Created',
+    'text' => 'New event has been created. Download the attachment to add event to your calendar.',
+  ),
+  'loginDashboard' => 'Login To Dashboard',
+  'thankyouNote' => 'Thank you for using our application!',
+  'hello' => 'Hello',
+  'whoops' => 'Whoops',
+  'regards' => 'Regards',
+  'footerLine' => 'If you’re having trouble clicking the :buttonText button, copy and paste the URL below
+into your web browser:',
+  'newExpense' =>
+  array(
+    'subject' => 'New Expense added',
+  ),
+  'expenseStatus' =>
+  array(
+    'subject' => 'Expense status updated',
+    'text' => 'Your expense status updated to',
+  ),
+    'resetPassword' =>
+        array(
+            'subject' => 'Reset Password Notification',
+            'text' => 'You are receiving this email because we received a password reset request for your account.',
+            'text2' => 'If you did not request a password reset, no further action is required.',
+            'button' => 'Reset Password',
+            'expire' => 'This password reset link will expire in :count minutes.',
+        ),
+  'newNotice' =>
+  array(
+    'subject' => 'New Notice Published',
+    'text' => 'New notice has been published.',
+  ),
+  'newProjectMember' =>
+  array(
+    'subject' => 'New Project Assigned',
+    'text' => 'You have been added as a member to the project',
+  ),
+  'newTask' =>
+  array(
+    'subject' => 'New Task Assigned to You',
+  ),
+  'newTicket' =>
+  array(
+    'subject' => 'New Support Ticket Requested by company ',
+    'text' => 'New Support Ticket is requested. Login to view the ticket.',
+  ),
+  'newSupportTicket' =>
+  array(
+    'subject' => 'New Ticket For Support Requested',
+    'text' => 'New  ticket for support is requested. Login to view the support ticket.',
+  ),
+ 'newTicketRequester' =>
+  array(
+    'subject' => 'New Ticket Has Been Generated For You.',
+    'text' => 'New support ticket is has been generated for you. Login to view the ticket.',
+  ),
+  'newSupportTicketRequester' =>
+  array(
+    'subject' => 'New Support Ticket Has Been Generated For You.',
+    'text' => 'New  ticket for support is has been generated for you. Login to view the ticket.',
+  ),
+  'newUser' =>
+  array(
+    'subject' => 'Welcome to',
+    'text' => 'Your account has been created successfully.',
+  ),
+  'leaves' =>
+  array(
+    'subject' => 'New leave request received',
+    'statusSubject' => 'Leave application status updated',
+  ),
+  'taskComplete' =>
+  array(
+    'subject' => 'Task marked as complete',
+  ),
+  'taskUpdate' =>
+  array(
+    'subject' => 'Task updated',
+  ),
+  'leave' =>
+  array(
+    'approve' => 'Leave application approved.',
+    'reject' => 'Leave application rejected.',
+    'applied' => 'Leave application applied.',
+  ),
+  'newClientTask' =>
+  array(
+    'subject' => 'New Task Generated',
+    'text' => 'New Task Generated on your project. Details are following :-',
+  ),
+  'newCompany' =>
+  array(
+    'subject' => 'New company registered',
+    'text' => 'New company has been registered.',
+  ),
+  'planUpdate' =>
+  array(
+    'subject' => 'Company updated plan.',
+    'text' => 'named company has been updated plan',
+  ),
+  'reminder' =>
+  array(
+    'subject' => 'Reminder for assigned task',
+  ),
+  'licenseExpirePre' =>
+  array(
+    'subject' => 'Company license Expiring soon.',
+    'text' => 'Your company license is expiring soon Please check your billing details.',
+  ),
+  'licenseExpire' =>
+  array(
+    'subject' => 'Company Licence expired.',
+    'text' => 'Your company license has been expired. Please check billing details.',
+  ),
+  'planPurchase' =>
+  array(
+    'subject' => 'Plan Purchase',
+    'text' => 'Company purchased plan',
+  ),
+  'cancelLicense' =>
+  array(
+    'subject' => 'License cancelled due to failed payment.',
+    'text' => 'License has been cancelled due to the failed payment. Please check the details.',
+  ),
+  'invoices' =>
+  array(
+    'paymentReceived' => 'Payment received for invoice.',
+    'offlinePaymentRequest' => 'Offline Payment Request.',
+  ),
+  'projectReminder' =>
+  array(
+    'subject' => 'Project Reminder',
+    'text' => 'This is to remind you about the due date of the following projects which is',
+  ),
+  'messages' =>
+  array(
+    'loginForMoreDetails' => 'Log in for more details.',
+  ),
+  'paymentReminder' =>
+  array(
+    'content' => 'This is to remind you about the due date of the following project invoice payment which is',
+    'subject' => 'Payment Reminder',
+  ),
+  'taskComment' =>
+  array(
+    'subject' => 'Comment posted on task',
+  ),
+  'removalRequestAdmin' =>
+  array(
+    'subject' => 'Data removal request',
+    'text' => 'Data removal request',
+  ),
+  'fileUpload' =>
+  array(
+    'subject' => 'New file uploaded to project : ',
+  ),
+  'emailVerify' =>
+  array(
+    'subject' => 'Email Verification',
+    'text' => 'Thank you for registration. Here are your email verification instructions. A request to your email verification has been made. If you did not make this request, simply ignore this email. If you did make this request, please verify ',
+  ),
+  'emailVerifySuccess' =>
+  array(
+    'subject' => 'Email Verification Success',
+    'text' => 'Thank you for email verification. Please click the below link to login',
+    'login' => 'Login'
+  ),
+  'discussionReply' =>
+  array(
+    'subject' => ' replied to ',
+    'text' => 'You have received a reply on ',
+  ),
+  'estimate' =>
+  array(
+    'subject' => ' New Estimate Created!',
+    'text' => 'A new estimate for your project has been created as attached. Please click on the link below to approve / reject the estimate. ',
+    'approveReject' => 'Approve / Reject'
+  ),
+  'chat' =>
+  array(
+    'text' => 'The introduction to the notification',
+    'notificationAction' => 'Notification Action',
+  ),
+  'push' =>
+  array(
+    'text' => 'The introduction to the notification',
+    'notificationAction' => 'Notification Action',
+  ),
+  'slack' =>
+  array(
+    'text' => 'The introduction to the notification',
+    'notificationAction' => 'Notification Action',
+  ),
+  'timerStart' =>
+  array(
+    'text' => 'The introduction to the notification',
+    'notificationAction' => 'Notification Action',
+  ),
+  'issue' =>
+  array(
+    'text' => 'The introduction to the notification',
+    'notificationAction' => 'Notification Action',
+  ),
+  'contract' =>
+  array(
+    'subject' => 'New Contract Created!',
+    'text' => 'A new contract has been created.',
+    'viewContract' => 'View Contract',
+  ),
+  'contractSign' =>
+  array(
+    'subject' => 'Contract Signed',
+    'text' => ':contract (contract) is signed by :client.',
+  ),
+  'invoice' =>
+  array(
+    'subject' => 'New Invoice Generated!',
+    'text' => 'A new invoice for your project has been created as attached. Please click on the link to view the invoice.',
+    'viewInvoice' => 'View Invoice',
+  ),
+  'payment' =>
+  array(
+    'subject' => 'Payment Received!',
+    'text' => 'Thank you. Payment is recorded. Please click on the link below to view/download the paid invoice.',
+  ),
+  'offlinePaymentAccept' =>
+  array(
+    'subject' => 'Offline payment verified',
+    'text' => 'Your request for :invoice_id is accepted.',
+  ),
+  'offlinePaymentReject' =>
+  array(
+    'subject' => 'Offline Payment rejected',
+    'text' => 'Your request for :invoice_id is rejected.',
+  ),
+  'offlinePackageChangeVerify' =>
+  array(
+    'subject' => 'Package change request verified',
+    'text' => 'Your request for plan change has been successfully verified.',
+    'message' => 'Now your package is :package'
+  ),
+  'offlinePackageChangeReject' =>
+  array(
+    'subject' => 'Package change request rejected',
+    'text' => 'Your request for :plan change has been rejected due to wrong details.',
+  ),
+  'offlinePackageChangeRequest' =>
+  array(
+    'subject' => 'Package change request',
+    'text' => ':company is requested for package change.',
+    'packageName' => 'Package Name',
+  ),
+  'proposal' =>
+  array(
+    'subject' => 'New Lead Proposal Sent!',
+    'text' => 'A new lead proposal has been sent to you. Login now to view the proposal.',
+  ),
+ 'clientProposal' =>
+  array(
+    'subject' => 'New Lead Proposal received!',
+    'text' => 'A new New Lead Proposal receive. Please click to view proposal to view the proposal.',
+    'viewProposal' => 'View Proposal',
+  ),
+  'test' =>
+  array(
+    'subject' => 'Test email!',
+    'text' => 'This is a test email.',
+  ),
+  'supportTicketReply' =>
+  array(
+    'subject' => 'Support Ticket Reply',
+    'text' => 'New reply received on support ticket ',
+  ),
+  'newChat' =>
+  array(
+    'subject' => 'New message received.',
+  ),
+  'discussion' =>
+  array(
+    'subject' => 'New discussion started ',
+  ),
+  'subTaskCreated' => 'Sub task created.',
+  'subTaskComplete' =>
+  array(
+    'subject' => 'Sub Task marked as complete',
+  ),
+  'ticketAgent' =>
+  array(
+    'subject' => 'You have been assigned a ticket',
+    'text' => 'You have been assigned a new ticket.',
+  ),
+ 'supportTicketAgent' =>
+  array(
+    'subject' => 'You have been assigned a support ticket',
+    'text' => 'You have been assigned a new support ticket.',
+  ),
+  'newContract' =>
+  array(
+    'subject' => 'New Contract Created!',
+    'text' => 'A new contract has been created.',
+  ),
+  'noticeUpdate' =>
+  array(
+    'subject' => 'A notice has been updated',
+    'text' => 'Notice has been updated. Login to view the notice.',
+  ),
+  'taskNote' =>
+  array (
+     'subject' => 'New note on task',
+  ),
+  'removalRequestApprovedUser' =>
+  array (
+     'subject' => 'Removal Request Approval',
+     'text' => 'Your removal request has been approved',
+  ),
+  'removalRequestRejectedUser' =>
+  array (
+     'subject' => 'Removal Request Rejected',
+     'text' => 'Your removal request has been rejected',
+  ),
+  'newExpenseRecurring' =>
+  array (
+      'subject' => 'New Recurring Expense submitted',
+  ),
+  'expenseRecurringStatus' =>
+  array (
+      'subject' => 'Recurring Expense status updated',
+      'text' => 'Your recurring expense status updated to',
+  ),
+  'newInvoiceRecurring' =>
+  array (
+      'subject' => 'New Recurring Invoice submitted',
+  ),
+  'invoiceRecurringStatus' =>
+  array (
+      'subject' => 'Recurring Invoice status updated',
+      'text' => 'Your recurring invoice status updated to',
+  ),
+  'creditNote' =>
+  array (
+      'subject' => 'New Credit Note received',
+      'text' => 'A new credit note has been received. Login now to view the credit note.',
+  ),
+  'leadAgent' => 
+  array (
+    'subject' => 'New lead received.',
+  ),
+  'newProject' =>
+  array(
+     'subject' => 'New Project Added',
+     'text' => 'A new project added with name.',
+     'withName' => 'with name.',
+     'loginNow' => ' Login now to view the project.',
+  ),
+  'projectFile' =>
+  array(
+     'subject' => 'New Files Added To Project',
+     'text' => 'New files added to project.',
+     'withName' => 'with name.',
+     'loginNow' => ' Login now to view the project files.',
+  ),
+  'productPurchase' =>
+  array(
+     'subject' => 'New Product Purchase Request',
+     'text' => 'New product purchase request by client name-.',
+     'loginNow' => ' Login now to view the product purchase invoice.',
+  ),
+  'rating' =>
+  array(
+     'subject' => 'Rating added on project',
+     'text' => 'Rating added on project-.',
+  ),
+  'ratingUpdate' =>
+  array(
+     'subject' => 'Rating updated on project',
+     'text' => 'Rating updated on project-.',
+  ),
+  'lead' =>
+  array(
+    'subject' => 'New lead Created!',
+    'text' => 'A new lead has been created.',
+    'withName' => 'With company name',
+  ),
+ 'proposalSigned' =>
+  array(
+    'subject' => 'Proposal has been accepted!',
+    'text' => 'A new Proposal has been accepted and signed.',
+    'approve' => 'Approved',
+  ),
+ 'proposalRejected' =>
+  array(
+    'subject' => 'Proposal has been declined by lead!',
+    'text' => 'A new Proposal has been declined.',
+    'rejected' => 'Rejected',
+  ),
+);
---
+++ help_text.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'projectName' => 'Enter the title of the project',
+    'projectCategorySettings' => 'Select the category of the project.',
+    'startDate' => 'Select the start date of project.',
+];
