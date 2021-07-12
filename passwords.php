59dd1d0bdf5066b608e6e1e0b77b85834ba781fc
emelonder <mbteor@gmail.com>
2021-07-12 17:05:11 +0300

    passwords.php
    passwords.php çevrildi.

--- passwords.php
+++ passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Şifreler en az altı karakterden oluşmalı ve onayla eşleşmelidir.',
+    'reset' => 'Şifreniz sıfırlandı!',
+    'sent' => 'Şifre sıfırlama bağlantınızı e-posta ile gönderdik!',
+    'token' => 'Bu parola sıfırlama belirteci geçersiz.',
+    'user' => "Bu e-posta adresine sahip bir kullanıcı bulamıyoruz.",
 
 ];
